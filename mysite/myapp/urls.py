2513d524a34ff7ccf8c65d03437489772eb46dfc
huberalberti <huberalberti@gmail.com>
2024-03-30 20:10:52 -0700

    Django tutorial first commit

---
+++ mysite/myapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+app_name = 'myapp'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('book/<int:book_id>/', views.detail, name='detail'),
+]
