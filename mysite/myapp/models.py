aae2c65c241add65ee9ef078b5c3406aab8aff5d
huberalberti <huberalberti@gmail.com>
2024-03-31 11:57:42 -0700

    Add boostrap and Image field

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -4,10 +4,11 @@ from django.db import models
 
 
 class Book(models.Model):
-    
+
     def __str__(self) -> str:
         return self.name
-    
+
     name = models.CharField(max_length=100)
     desc = models.CharField(max_length=300)
     price = models.IntegerField()
+    book_image = models.ImageField(default="default.jpg", upload_to="book_images/")
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
 
+
 # Create your views here.
 
 
